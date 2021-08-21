c8f87dc55057963f844a65a83220c6380c02d535
Pawan Bhandarkar <pawankbhandarkar@gmail.com>
2021-08-22 01:00:57 +0530

    Basic UI working

--- components/header.tsx
+++ components/header.tsx
@@ -6,10 +6,12 @@ const Header = () => {
   return (
     <div className="w-full md:w-2/3 md:h-1/5 h-1/6 md:px-8 p-4 border-4 border-headerOutline bg-transparent rounded-xl">
       <div className="flex justify-between  h-full">
-        <div className="flex flex-col justify-center w-2/5 text-left font-semibold text-white bg-transparent md:text-4xl text-2xl ">
-          <p className="md:leading-7 leading-5">ROCK</p>
-          <p className="md:leading-7 leading-5">PAPER</p>
-          <p className="md:leading-7 leading-5">SCISSORS</p>
+        <div className="flex flex-col justify-center w-2/5 text-left font-semibold text-white bg-transparent md:text-2xl text-2xl ">
+          <p className="leading-5">ROCK</p>
+          <p className="leading-5">PAPER</p>
+          <p className="leading-5">SCISSORS</p>
+          <p className="leading-5">LIZARD</p>
+          <p className="leading-5">SPOCK</p>
         </div>
         <div className="flex flex-col justify-center items-center tracking-widest text-scoreText md:px-12 px-8 bg-white rounded-md">
           <p>SCORE</p>
