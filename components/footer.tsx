09778fb7de36a0715e5c2d8406887bb76047f79a
BhandarkarPawan <pawankbhandarkar@gmail.com>
2023-09-03 21:03:59 -0700

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <footer className="brand name font-thin w-screen overflow-hidden absolute bottom-0 flex justify-center bg-darkText h-10 items-center">
-            Designed and Developed by &nbsp;
+            Developed by &nbsp;
             <a className="text-yellow-400" href="https://bhandarkar.me">
                 <img
                     src="https://bhandarkar.me/images/logo.png"
