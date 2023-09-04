3b1859b5a55678b4abe5747773b64ec912e6bfef
BhandarkarPawan <pawankbhandarkar@gmail.com>
2023-09-03 20:21:04 -0700

    Fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,21 +1,15 @@
-import React from 'react';
-import Image from 'next/image';
 export const Footer = () => {
-  return (
-    <a
-      href="https://theprosecode.com"
-      className="w-screen overflow-hidden absolute bottom-0 flex justify-center bg-darkText h-10 items-center"
-    >
-      <Image
-        src="/images/logo.png"
-        alt="TheProseCode"
-        height={50}
-        width={50}
-      ></Image>
-      <div className="brand flex items-end">
-        <p>The Prose Code</p>
-        <p className="ml-8 brand name">Pawan Bhandarkar © 2021</p>
-      </div>
-    </a>
-  );
+    return (
+        <footer className="brand name font-thin w-screen overflow-hidden absolute bottom-0 flex justify-center bg-darkText h-10 items-center">
+            Designed and Developed by &nbsp;
+            <a className="text-yellow-400" href="https://bhandarkar.me">
+                <img
+                    src="https://bhandarkar.me/images/logo.png"
+                    alt="Pawan Bhandarkar"
+                    className="h-6 w-6 inline"
+                />
+                &nbsp;Pawan Bhandarkar
+            </a>
+        </footer>
+    );
 };
