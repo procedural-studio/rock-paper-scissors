cdd0dd86b90c462a1729bb4b533250c5db3ebfbe
10dimensions <rndaorg@gmail.com>
2025-02-19 13:04:12 +0000

    Initialize capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rpsls.app',
+  appName: 'rpsls',
+  webDir: 'public'
+};
+
+export default config;
